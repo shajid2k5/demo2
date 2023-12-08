8fe764756d312ce00ae25742d24a72d8d71db8a0
shajid <shajid2k5@gmail.com>
2023-12-09 03:01:09 +0530

    commit for github file

---
+++ addition.py
@@ -0,0 +1,10 @@
+# Get input from the user
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Add the numbers
+sum_result = num1 + num2
+
+# Display the result
+print("The sum of {} and {} is: {}".format(num1, num2, sum_result))
+
